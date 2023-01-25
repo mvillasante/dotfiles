75c310379df73b6350656ce7895e1e182c532d77
Mario Villasante • GECI <42744723+mvillasante@users.noreply.github.com>
2023-01-25 10:08:15 -0600

    Add superuser privileges

--- src/install_neovim.sh
+++ src/install_neovim.sh
@@ -8,9 +8,9 @@ set -euo pipefail
 # Install latest Neovim from GitHub
 cd "$HOME"
 wget --directory-prefix="$HOME" https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
-apt remove neovim neovim-runtime --yes
-apt install unzip --yes
-apt install ./nvim-linux64.deb
+sudo apt remove neovim neovim-runtime --yes
+sudo apt install unzip --yes
+sudo apt install ./nvim-linux64.deb
 
 # Install packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/packer/start/packer.nvim
