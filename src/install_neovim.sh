14c18a4bfdf7e36cd437ef93c3bd611541a6dacc
Mario Villasante <mario.villasante@islas.org.mx>
2023-04-11 08:17:17 -0700

    Install nvim from appimage

--- src/install_neovim.sh
+++ src/install_neovim.sh
@@ -6,11 +6,11 @@
 set -euo pipefail
 
 # Install latest Neovim from GitHub
+apt remove neovim neovim-runtime --yes
 cd "$HOME"
-wget --directory-prefix="$HOME" https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
-sudo apt remove neovim neovim-runtime --yes
-sudo apt install unzip --yes
-sudo apt install ./nvim-linux64.deb
+wget --directory-prefix="$HOME" https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+chmod u+x "$HOME"/nvim.appimage
+"$HOME"/nvim.appimage --appimage-extract
 
 # Install packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/packer/start/packer.nvim
