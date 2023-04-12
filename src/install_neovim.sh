6b9b1508e4968404fa65967e508d1a16b9f3331e
Mario Villasante <mario.villasante@islas.org.mx>
2023-04-12 08:12:54 -0700

    Expose nvim globally

--- src/install_neovim.sh
+++ src/install_neovim.sh
@@ -11,6 +11,8 @@ cd "$HOME"
 wget --directory-prefix="$HOME" https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod u+x "$HOME"/nvim.appimage
 "$HOME"/nvim.appimage --appimage-extract
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 
 # Install packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/packer/start/packer.nvim
